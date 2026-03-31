e5993d925313932ea253adf26f215a166d755e4f
dulabdul <dulabdol1331@gmail.com>
2026-03-31 23:01:12 +0700

    fix: fix publisher url image on blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sanity from '@sanity/astro';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://arvellocreative.com',
   // 1. TIDAK PERLU lagi menuliskan output: 'hybrid' atau 'static'.
   // Astro 5+ sudah otomatis menangani ini.
 
